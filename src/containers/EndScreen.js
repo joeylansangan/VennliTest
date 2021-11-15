48f252a10a8262db07289db5d92ca8efa6836475
Joey Lansangan <joeylansangan@gmail.com>
2021-11-15 04:54:21 -0600

    feat: calculate and render score

--- src/containers/EndScreen.js
+++ src/containers/EndScreen.js
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 
 function EndScreen({results, questions, onReset}) {
+    const [score, setScore] = useState(0);
 
+    //calculate score on first render/mount
+    useEffect(() => {
+        let currentScore = 0;
+        results.forEach((result, i) => {
+            if(result.a === questions[i].correct_answer){
+                currentScore++
+            }
+        });
+        setScore(currentScore)
+    },[])
+    
     return (
         <div>
-            <h2>End game</h2>
+            <div>
+                <h4>You Scored</h4>
+                <h2>{score} / {questions.length}</h2>
+            </div>
             <section>
                 <ul>
                     {results.map((result, i) => (
