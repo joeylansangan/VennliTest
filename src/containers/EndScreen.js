8a4ed0c5a85694f76cb42691994175e14413385f
Joey Lansangan <joeylansangan@gmail.com>
2021-11-15 09:08:55 -0600

    style: update end screen styling

--- src/containers/EndScreen.js
+++ src/containers/EndScreen.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { AiOutlineCheck, AiOutlineClose } from "react-icons/ai";
 
 function EndScreen({results, questions, onReset}) {
     const [score, setScore] = useState(0);
@@ -16,22 +17,26 @@ function EndScreen({results, questions, onReset}) {
     },[])
     
     return (
-        <div>
-            <div>
-                <h4>You Scored</h4>
-                <h2>{score} / {questions.length}</h2>
+        <div className="py-8 px-4">
+            <div className="mb-8">
+                <h4 className="uppercase text-xl font-semibold">You Scored</h4>
+                <h2 className="font-semibold text-3xl text-green-500">{score} / {questions.length}</h2>
             </div>
             <section>
                 <ul>
                     {results.map((result, i) => (
-                        <li key={i}>
-                           <h3 dangerouslySetInnerHTML={{ __html: result.q}}/>
-                           <span>Your Answer: {result.a}</span>
+                        <li key={i} className="mb-6">
+                            <div className="flex border border-solid items-center justify-evenly p-2">
+                                {(result.a === questions[i].correct_answer) ? <AiOutlineCheck/> : <AiOutlineClose/>}
+                                <h3 className="w-4/5" dangerouslySetInnerHTML={{ __html: result.q}}/>
+                            </div>
+                            <span className="text-xs text-gray-400">Your Answer: {result.a}</span>
                         </li>
                     ))}
                 </ul>
             </section>
             <button
+                className="menu-btn m-10 uppercase"
                 onClick={onReset}
             >
                 play again
